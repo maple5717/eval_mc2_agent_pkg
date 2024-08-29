dbcbb02f78e0d2268b4bed1eba0566087c7a0afb
maple5717 <momiji5717@gmail.com>
2024-08-29 18:41:42 +0800

    update pose with tf topic

--- scripts/eval_mc_agent.py
+++ scripts/eval_mc_agent.py
@@ -6,11 +6,11 @@ from utils.config_utils import (
     get_habitat_config,
     get_omega_config,
 )
+from mc2.agent.mc2_agent.mc2_agent import MC2Agent
 from home_robot.core.interfaces import DiscreteNavigationAction, Observations 
 
 import argparse
 import os
-from mc2.agent.mc2_agent.mc2_agent import MC2Agent
 
 os.environ["OPENBLAS_NUM_THREADS"] = "1"
 os.environ["NUMEXPR_NUM_THREADS"] = "1"
@@ -22,82 +22,84 @@ from message_filters import ApproximateTimeSynchronizer, Subscriber
 from evaluators import Evaluator
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--evaluation_type",
-        type=str,
-        choices=["local", "local_vectorized", "remote"],
-        default="local",
-    )
-    parser.add_argument("--num_episodes", type=int, default=None)
-    parser.add_argument(
-        "--habitat_config_path",
-        type=str,
-        default="ovmm/ovmm_eval.yaml",
-        help="Path to config yaml",
-    )
-    parser.add_argument(
-        "--baseline_config_path",
-        type=str,
-        default="projects/habitat_ovmm/configs/agent/heuristic_agent.yaml",
-        help="Path to config yaml",
-    )
-    parser.add_argument(
-        "--env_config_path",
-        type=str,
-        default="projects/habitat_ovmm/configs/env/hssd_eval.yaml",
-        help="Path to config yaml",
-    )
-    parser.add_argument(
-        "--agent_type",
-        type=str,
-        default="baseline",
-        choices=["baseline", "random", "explore"],
-        help="Agent to evaluate",
-    )
-    parser.add_argument(
-        "--force_step",
-        type=int,
-        default=20,
-        help="force to switch to new episode after a number of steps",
-    )
-    parser.add_argument(
-        "--data_dir",
-        type=str,
-        default=None,
-        help="whether to save obseration history for data collection",
-    )
-    parser.add_argument(
-        "overrides",
-        default=None,
-        nargs=argparse.REMAINDER,
-        help="Modify config options from command line",
-    )
-    args = parser.parse_args()
+    agent = 0
+    if True:
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "--evaluation_type",
+            type=str,
+            choices=["local", "local_vectorized", "remote"],
+            default="local",
+        )
+        parser.add_argument("--num_episodes", type=int, default=None)
+        parser.add_argument(
+            "--habitat_config_path",
+            type=str,
+            default="ovmm/ovmm_eval.yaml",
+            help="Path to config yaml",
+        )
+        parser.add_argument(
+            "--baseline_config_path",
+            type=str,
+            default="projects/habitat_ovmm/configs/agent/heuristic_agent.yaml",
+            help="Path to config yaml",
+        )
+        parser.add_argument(
+            "--env_config_path",
+            type=str,
+            default="projects/habitat_ovmm/configs/env/hssd_eval.yaml",
+            help="Path to config yaml",
+        )
+        parser.add_argument(
+            "--agent_type",
+            type=str,
+            default="baseline",
+            choices=["baseline", "random", "explore"],
+            help="Agent to evaluate",
+        )
+        parser.add_argument(
+            "--force_step",
+            type=int,
+            default=20,
+            help="force to switch to new episode after a number of steps",
+        )
+        parser.add_argument(
+            "--data_dir",
+            type=str,
+            default=None,
+            help="whether to save obseration history for data collection",
+        )
+        parser.add_argument(
+            "overrides",
+            default=None,
+            nargs=argparse.REMAINDER,
+            help="Modify config options from command line",
+        )
+        args = parser.parse_args()
 
-    # get habitat config
-    habitat_config, _ = get_habitat_config(
-        args.habitat_config_path, overrides=args.overrides
-    )
+        # get habitat config
+        habitat_config, _ = get_habitat_config(
+            args.habitat_config_path, overrides=args.overrides
+        )
 
-    # get baseline config
-    baseline_config = get_omega_config(args.baseline_config_path)
+        # get baseline config
+        baseline_config = get_omega_config(args.baseline_config_path)
 
-    # get env config
-    env_config = get_omega_config(args.env_config_path)
+        # get env config
+        env_config = get_omega_config(args.env_config_path)
 
-    # merge habitat and env config to create env config
-    env_config = create_env_config(
-        habitat_config, env_config, evaluation_type=args.evaluation_type
-    )
+        # merge habitat and env config to create env config
+        env_config = create_env_config(
+            habitat_config, env_config, evaluation_type=args.evaluation_type
+        )
 
-    # merge env config and baseline config to create agent config
-    agent_config = create_agent_config(env_config, baseline_config)
+        # merge env config and baseline config to create agent config
+        agent_config = create_agent_config(env_config, baseline_config)
 
-    device_id = env_config.habitat.simulator.habitat_sim_v0.gpu_device_id
+        device_id = env_config.habitat.simulator.habitat_sim_v0.gpu_device_id
 
-    # create agent
-    agent = MC2Agent(agent_config, device_id=device_id)
+        # create agent
+        agent = MC2Agent(agent_config, device_id=device_id)
 
     evaluator = Evaluator(agent)
     evaluator.run()
--- scripts/evaluators.py
+++ scripts/evaluators.py
@@ -21,38 +21,50 @@ from geometry_msgs.msg import Pose
 
 
 def quaternion_to_rpy(q):
-    x, y, z, w = q.x, q.y, q.z, q.w
+    if isinstance(q, list):
+        x, y, z, w = q
+    else:
+        x, y, z, w = q.x, q.y, q.z, q.w
+
     # Convert quaternion to roll, pitch, yaw (RPY)
     roll, pitch, yaw = tft.euler_from_quaternion([x, y, z, w])
     return roll, pitch, yaw
 
-
+# 2>&1 | grep -Ev "TF_REPEATED_DATA|278"
 class Evaluator:
-    def __init__(self, agent, action_freq=1/5):
+    def __init__(self, agent, action_freq=1/10):
+        # rospy.set_param('/use_sim_time', True)
         rospy.init_node('mc2_agent_evaluator', anonymous=True)
         self.camera_height = 1.394
         self.agent = agent  
         self.obs = self.__load_init_obs()
         self.start = False
         self.last_received_time = rospy.Time.now()
-
+        self.use_agent = True
         
         self.bridge = CvBridge()
 
         # subscribers
         rgb_sub = Subscriber('/camera/camera/color/image_raw', Image)
         depth_sub = Subscriber('/camera/camera/aligned_depth_to_color/image_raw', Image)
-        odom_sub = Subscriber('/vins_estimator/odometry', Odometry)
+        
         self.tf_listener = tf.TransformListener()
         rospy.Timer(rospy.Duration(1 / action_freq), self.__agent_callback)
         rospy.Timer(rospy.Duration(1), self.__watchdog_callback)
+        rospy.Timer(rospy.Duration(0.01), self.__tf_callback) 
+
+        self.source_frame = 'map'
+        self.target_frame = 'base_link'
 
         # Synchronizer with a queue size of 10 and 0.1s time tolerance
-        self.ats = ApproximateTimeSynchronizer([rgb_sub, depth_sub, odom_sub], queue_size=100, slop=0.05)
+        self.ats = ApproximateTimeSynchronizer([rgb_sub, depth_sub], queue_size=100, slop=0.05)
         self.ats.registerCallback(self.__rgbd_callback)
 
+
+
     def run(self):
-        self.agent.reset()
+        if self.use_agent:
+            self.agent.reset()
         self.last_received_time = rospy.Time.now()
         rospy.loginfo("Evaluator is ready to receive commands! ")
         rospy.spin()
@@ -63,7 +75,20 @@ class Evaluator:
             
     def __agent_callback(self, event):
         if self.start:
-            self.agent.act(self.obs)
+            if self.use_agent:
+                self.agent.act(self.obs)
+
+    def __tf_callback(self, event):    
+        # Get the transformation between the frames
+        try:
+            (trans, q) = self.tf_listener.lookupTransform(self.source_frame, self.target_frame, rospy.Time(0))
+
+            theta = quaternion_to_rpy(q)[2]
+            self.obs.gps = np.array(trans)
+            self.obs.compass = np.array([theta])
+
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
+                rospy.logerr("Error while looking up transform: %s", str(e))
 
     def __load_init_obs(self):
         obs = Observations(gps=[0,0], compass=[0], rgb=[0], depth=[0]) 
@@ -77,7 +102,9 @@ class Evaluator:
         obs.xyz = np.ones([640, 480])
         return obs
     
-    def __get_cam_pose(self, x, y, theta):
+    def __get_cam_pose(self):
+        x, y = self.obs.gps[0], self.obs.gps[1]
+        theta = self.obs.compass[0]
         robot_pose = np.eye(4)
 
         cos_theta = np.cos(theta)
@@ -95,29 +122,19 @@ class Evaluator:
                               [0,0,-1,0],
                               [1,0,0,0],
                               [0,0,0,1]], dtype=np.float32).T
-        # print("____________")
-        # print(robot_pose)
-        # print(robot2cam)
-        # print(robot_pose @ robot2cam)
+
         cam_pose = robot_pose @ robot2cam
         return cam_pose
 
-    def __obs_generation(self, rgb_img, depth_img, pose): 
+    def __obs_generation(self, rgb_img, depth_img): 
         self.obs.rgb = rgb_img.astype(np.uint8)
         self.obs.depth = depth_img.astype(np.float32)
 
-        pos = pose.position
-        orn_q = pose.orientation 
-        theta = quaternion_to_rpy(orn_q)[2]
-
-        self.obs.gps = np.array([pos.x, pos.y])
-        self.obs.compass = np.array([theta])
-
-        self.obs.camera_pose = self.__get_cam_pose(pos.x, pos.y, theta + np.pi/2)
+        self.obs.camera_pose = self.__get_cam_pose()
         
         # self.agent.act(self.obs)
 
-    def __rgbd_callback(self, rgb_msg, depth_msg, odom):
+    def __rgbd_callback(self, rgb_msg, depth_msg):
         """
             Generate the observation instance given RGBD
         """
@@ -129,11 +146,15 @@ class Evaluator:
             rospy.logerr("CvBridge Error: {0}".format(e))
 
         depth_img = depth_img.astype(np.float32) * 0.001
+        
         # print(depth_img.min(), depth_img.max())
-        self.__obs_generation(rgb_img, depth_img, odom.pose.pose)
         # if "y" == input("continue?")
         #     agent.run()
+
+        self.__obs_generation(rgb_img, depth_img)
+        # print(self.trans)
         self.start = True
         self.last_received_time = rospy.Time.now()
-
+        
+        
     
